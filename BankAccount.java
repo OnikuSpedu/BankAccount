ac78f6013ef52ebfa7754ac0ce251fc28df09de7
OnikuSpedu <srakibnyc1@gmail.com>
2020-10-27 15:49:53 -0400

    Created Fields for Instance and Implemented Contructor.

---
+++ BankAccount.java
@@ -0,0 +1,14 @@
+public class BankAccount {
+
+    private double balance;
+    private int accountID;
+    private String password;
+
+    public BankAccount( int id, String pw) {
+        balance = 0;
+        accountID = id;
+        password = pw;
+    }
+
+    
+}
