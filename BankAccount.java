d9124bf7dab76f931914a554eee98613fa32d36d
OnikuSpedu <srakibnyc1@gmail.com>
2020-10-29 13:43:45 -0400

    Added authentication and transferTo methods. Improved tests.

--- BankAccount.java
+++ BankAccount.java
@@ -4,10 +4,10 @@ public class BankAccount {
     private int accountID;
     private String password;
 
-    public BankAccount( int id, String pw) {
+    public BankAccount( int accountID, String password) {
         balance = 0;
-        accountID = id;
-        password = pw;
+        this.accountID = accountID;
+        this.password = password;
     }
 
     public double getBalance() {
@@ -20,16 +20,16 @@ public class BankAccount {
 
     public String toString() {
         String str = "";
-        str += accountID + "\t" + balance;
+        str += "#" + accountID + "\t" + "#" + balance;
         return str;
     }
 
-    public boolean isPassword(String testPass) {
-        return testPass.equals(password);
+    public boolean authenticate(String password) {
+        return this.password.equals(password);
     }
     
-    public void setPassword(String newPass) {
-        password = newPass;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public boolean deposit(double amount) {
@@ -48,6 +48,34 @@ public class BankAccount {
             withdrew = true;
         }
         return withdrew;
+    }
+
+    public boolean transferTo(BankAccount other, double amount, String password) {
+        System.out.println("Attempt to move $"+amount+" from the " + getAccountID() + " to " + other.getAccountID());
+        if (authenticate(password)) {
+            System.out.println("Authentication Success");
+            if(withdraw(amount)){
+                if(other.deposit(amount)){
+                    System.out.println(this);
+                    System.out.println(other);
+                    System.out.println();
+                    return true;
+                }else{
+                    //This should never happen.
+                    //Error message provided to make sure that is the case
+                    System.out.println("CRITICAL ERROR! $"+amount + 
+                                        " withdrawn from Account #" +  this.getAccountID() + 
+                                        "Failed to add to Account #"+ other.getAccountID());
+                }
+            } 
+            else{
+            System.out.println("Transfer Failed");
+            }
+        } else {
+            System.out.println("Authentication Failed");
+        }
+            
+        return false;
     }
 
 }
--- Driver.java
+++ Driver.java
@@ -11,7 +11,7 @@ public class Driver {
         System.out.println("Password Changed");
         account.setPassword("still a bad password");
         System.out.println("Verify Password Changed");
-        System.out.println(account.isPassword("still a bad password"));
+        System.out.println(account.authenticate("still a bad password"));
         System.out.println("Attempt to withdraw 10 with a balance of 0");
         System.out.println(account.withdraw(10));
         System.out.println("toString - Account Info");
@@ -24,5 +24,30 @@ public class Driver {
         System.out.println(account.withdraw(10));
         System.out.println("toString - Account Info");
         System.out.println(account.toString());
+
+
+        //Transfer Tests
+        System.out.println("Transfer Tests");
+        BankAccount b1 = new BankAccount(1000001, "abc123");
+        BankAccount b2 = new BankAccount(1000231, "abcasdg23");
+  
+        System.out.println("Account b1:");
+        b1.deposit(3000.0);
+        b2.deposit(1234.0);
+        System.out.println(b1);
+        System.out.println("Account b2:");
+        System.out.println(b2);
+        System.out.println();
+  
+        
+        double cashAmount = 300.0;
+
+        while(cashAmount < 3000){
+            //Incorrect password. Authentication should fail.
+            b1.transferTo(b2, cashAmount, "wsjdjsfbjbD");
+            //Correct Password. Authentication should a success.
+            b1.transferTo(b2, cashAmount, "abc123");
+            cashAmount *= 2;
+        }
     }
 }
